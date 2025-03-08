d76f3aef947f8b0edb3018eba19d235ea41996eb
Miguel Angel Tomairo Mendez <matomairo@gmail.com>
2025-03-08 00:15:33 -0500

    fix: refactor version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,5 +64,5 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
-
+    implementation(libs.androidx.compiler)
 }
